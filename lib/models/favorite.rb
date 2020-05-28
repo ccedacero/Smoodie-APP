a3eed92b5508bbded65ebd00f36090d696be2f56
github username <github email address>
2020-05-28 15:00:32 -0400

    add update and destroy methods

--- lib/models/favorite.rb
+++ lib/models/favorite.rb
@@ -5,10 +5,12 @@ class Favorite < ActiveRecord::Base
   def self.add_to_favorite(user)
     prompt = TTY::Prompt.new
     user_smoothie_choices = Recipe.all.map { |recipe| recipe.name_of_recipe }
-    binding.pry
+    # binding.pry
     user_choice = prompt.select("choose the smoothie you'd like to add to your favorites:", (user_smoothie_choices))
     id = Recipe.find_by(name_of_recipe: user_choice).id
     Favorite.create(recipe_id: id, user_id: user.id)
+    puts "You've added #{user_choice} to your favorite's list!"
+    sleep 2
   end
 end
 
--- lib/models/interface.rb
+++ lib/models/interface.rb
@@ -37,14 +37,19 @@ class Interface
   def main_menu(user)
     # puts "Hello, welcome to Smoodie!"
     #{user.username}
-    log_in_greeting = prompt.select("What would you like to do today?", ["See My Favorite Recipes", "Get Smoothie Recommendation", "Add Smoothie to Favorites", "Logout"])
+    log_in_greeting = prompt.select("What would you like to do today?", ["See My Favorite Recipes", "Get Smoothie Recommendation", "Add Smoothie to Favorites", "Delete Smoothie From Favorites", "Logout"])
     loading()
     if log_in_greeting == "See My Favorite Recipes"
       display_favorites(user)
-    elsif log_in_greeting == "Recommend a Smoothie"
+    elsif log_in_greeting == "Get Smoothie Recommendation"
       display()
+      navigation_menu(user)
     elsif log_in_greeting == "Add Smoothie to Favorites"
       Favorite.add_to_favorite(user)
+      navigation_menu(user)
+    elsif log_in_greeting == "Delete Smoothie From Favorites"
+      destroy_smoothie(user)
+      navigation_menu(user)
     elsif log_in_greeting == "Logout"
       puts "Good Bye!"
       puts "Enjoy your smoothie!!"
@@ -67,13 +72,14 @@ class Interface
     user_choice = prompt.select("Please choose one to see the secret recipe:", (user_smoothie_choices))
     smoothie_recipe = recipes.find_by(name_of_recipe: user_choice)
     display_smoothie_info(smoothie_recipe)
-    navigation_menu()
+    navigation_menu(user)
   end
 
   def navigation_menu(user)
     additiona_choice = prompt.select("What would you like to do next?", ["GO BACK", "EXIT"])
     if additiona_choice == "GO BACK"
       cl()
+      # binding.pry
       main_menu(user)
     elsif additiona_choice == "EXIT"
       puts "GOOD BYE!.............." + "\n" + "ENJOY YOUR SMOOTHIE!"
@@ -83,16 +89,49 @@ class Interface
   def display_favorites(user)
     user_favs = User.all.find_by(username: user.username).recipes
     puts "Here are your previously favorited smoothies!"
-    favorite_smoothie = prompt.select("Choose an option to see their recipe", [user_favs[0].name_of_recipe, user_favs[1].name_of_recipe, user_favs[2].name_of_recipe])
-    if favorite_smoothie == user_favs[0].name_of_recipe
-      display_smoothie_info(user_favs[0])
-    elsif favorite_smoothie == user_favs[1].name_of_recipe
-      display_smoothie_info(user_favs[1])
-    elsif favorite_smoothie == user_favs[2].name_of_recipe
-      display_smoothie_info(user_favs[2])
+    if user_favs.count > 2
+      favorite_smoothie = prompt.select("Choose an option to see their recipe", [user_favs[0].name_of_recipe, user_favs[-2].name_of_recipe, user_favs[-1].name_of_recipe])
+      if favorite_smoothie == user_favs[0].name_of_recipe
+        display_smoothie_info(user_favs[0])
+      elsif favorite_smoothie == user_favs[1].name_of_recipe
+        display_smoothie_info(user_favs[1])
+      elsif favorite_smoothie == user_favs[2].name_of_recipe
+        display_smoothie_info(user_favs[2])
+      end
+    elsif user_favs.count == 1
+      favorite_smoothie = prompt.select("Choose an option to see their recipe", [user_favs[0].name_of_recipe])
+      if favorite_smoothie == user_favs[0].name_of_recipe
+        display_smoothie_info(user_favs[0])
+      end
     end
     navigation_menu(user)
   end
+
+  def destroy_smoothie(user)
+    user_favs = User.all.find_by(username: user.username).recipes
+    if user_favs.count > 2
+      delete_smoothie = prompt.select("Which of your favorite smoothies would you like to remove from your favorites?", [user_favs[0].name_of_recipe, user_favs[-2].name_of_recipe, user_favs[-1].name_of_recipe])
+      if delete_smoothie == user_favs[0].name_of_recipe
+        binding.pry
+        smoothie_id = user.recipes.find_by(name_of_recipe: delete_smoothie).id
+        fave_id = Favorite.find_by(recipe_id: smoothie_id).id
+        Favorite.destroy(fave_id)
+        puts "Smoothie #{delete_smoothie} has been removed from your favorites"
+        sleep 2
+      elsif delete_smoothie == user_favs[1].name_of_recipe
+        display_smoothie_info(user_favs[1])
+      elsif delete_smoothie == user_favs[2].name_of_recipe
+        display_smoothie_info(user_favs[2])
+      end
+    elsif user_favs.count == 1
+      delete_smoothie = prompt.select("Which of your favorite smoothies would you like to remove from your favorites?", [user_favs[0].name_of_recipe])
+      if delete_smoothie == user_favs[0].name_of_recipe
+        display_smoothie_info(user_favs[0])
+      end
+    end
+  end
+
+  # def destroy_helper
 
   def display_smoothie_info(recipe)
     puts "Smoothie Name" + "\n"
